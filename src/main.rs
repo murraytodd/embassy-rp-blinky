f995dc2775c9246b04fa44ddba6b6e36c7d7212a
Murray Todd Williams <murray.williams@accenture.com>
2024-06-26 15:18:36 -0500

    onboard LED blinky works

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 #![no_std]
 #![no_main]
 
+use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{self, Level, Output};
+use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_25, PIO0};
+use embassy_rp::pio::{InterruptHandler as PioInterruptHandler, Pio};
 use embassy_time::{Duration, Timer};
-use gpio::{Level, Output};
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => PioInterruptHandler<PIO0>;
+});
 
 #[cortex_m_rt::pre_init]
 unsafe fn before_main() {
@@ -15,19 +23,62 @@ unsafe fn before_main() {
     embassy_rp::pac::SIO.spinlock(31).write_value(1);
 }
 
+#[embassy_executor::task]
+async fn wifi_task(
+    runner: cyw43::Runner<
+        'static,
+        Output<'static, PIN_23>,
+        PioSpi<'static, PIN_25, PIO0, 0, DMA_CH0>,
+    >,
+) -> ! {
+    runner.run().await
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     info!("Program start");
     let p = embassy_rp::init(Default::default());
     let mut led = Output::new(p.PIN_22, Level::Low);
-    
+
+    // Configure PIO and CYW43
+
+    let fw = include_bytes!("../../cyw43-firmware/43439A0.bin");
+    let clm = include_bytes!("../../cyw43-firmware/43439A0_clm.bin");
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
+
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    unwrap!(spawner.spawn(wifi_task(runner)));
+
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+    let wifi_ssid = env!("WIFI_SSID");
+    let wifi_password = env!("WIFI_PASSWORD");
+
     loop {
-        info!("led on!");
+        info!("external LED on, onboard LED off!");
         led.set_high();
+        control.gpio_set(0, false).await;
         Timer::after(Duration::from_secs(1)).await;
 
-        info!("led off!");
+        info!("external LED off, onboard LED on!");
         led.set_low();
+        control.gpio_set(0, true).await;
         Timer::after(Duration::from_secs(1)).await;
     }
 }
